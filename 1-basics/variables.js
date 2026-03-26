1dbf4014eceb33a0cc3b2c264e19aa981867d233
akhil911-rs <clayjenseen72@gmail.com>
2026-03-26 16:55:58 +0000

    variables

---
+++ 1-basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 654321
+let accountEmail = "alex@gmail.com"
+var accountPasswd = "91130"
+
+accountCity = "Leon"
+let accountState;
+
+//  accountId = 2 //
+
+
+// accountEmail = "bill@gmail.com"
+
+console.table([accountId, accountEmail, accountPasswd, accountCity, accountState]);
+ 
