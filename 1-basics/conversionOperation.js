aabb7b80cdf18f16255d848eed1c558843d54bbc
akhil911-rs <clayjenseen72@gmail.com>
2026-03-27 08:00:40 +0000

    Conversion of datatypes

---
+++ 1-basics/conversionOperation.js
@@ -0,0 +1,31 @@
+let score = null
+
+ // console.log(typeof score);
+ // console.log(typeof (score));
+
+let valueInNumber = Number(score)
+ // onsole.log(typeof valueInNumber);
+ // console.log(valueInNumber);
+
+
+
+// "33" => 33
+// "33abc" => Nan
+// true = > 1; false => 0
+
+// let isLoggedIn = "Akhil"
+
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Akhil" => true
+
+
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
