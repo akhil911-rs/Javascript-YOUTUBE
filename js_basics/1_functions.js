cf7eae7c2f1a382edb460ed675806733c4d518af
akhil911-rs <clayjenseen72@gmail.com>
2026-04-02 06:43:03 +0000

    functions part 2

--- js_basics/1_functions.js
+++ js_basics/1_functions.js
@@ -1,48 +1,91 @@
-function sayMyName () {
-    console.log("A");
-    console.log("k");
-    console.log("h");
-    console.log("i");
-    console.log("l");
-}
+// function sayMyName () {
+//     console.log("A");
+//     console.log("k");
+//     console.log("h");
+//     console.log("i");
+//     console.log("l");
+// }
 
-// sayMyName()
+// // sayMyName()
+
+
+// // function addTwoNumbers(num1, num2) {
+
+// // console.log(num1 + num2);
+
+// // }
 
 
 // function addTwoNumbers(num1, num2) {
 
-// console.log(num1 + num2);
+// // let result = num1 + num2
+// // console.log("Akhil")
+// // return result
+
+// return num1 + num2
+// }
+
+// const result = addTwoNumbers(7, 55)
+
+// // console.log("Result: ", result);
+
+
+
+
+// function loginUserMsg (username = "sam"){
+// // if (username === undefined){
+// if (!username){
+//     console.log("Please enter a username");
+//     return
 
 // }
 
+// return `${username} just logged in.`
 
-function addTwoNumbers(num1, num2) {
 
-// let result = num1 + num2
-// console.log("Akhil")
-// return result
+// }
+// console.log (loginUserMsg(""))
+// console.log (loginUserMsg())
 
-return num1 + num2
+
+//////////////////////////////////////////////////////////////////////////////////////////////2part
+
+
+
+function calculateCartPrice ( val1, val2, ...num1) {
+    return num1
 }
 
-const result = addTwoNumbers(7, 55)
+// console.log(calculateCartPrice(200, 400, 500, 2000))
 
-// console.log("Result: ", result);
+const user = {
+    username: "Akhil",
+    price: "199",
+}
 
+function handleObject(anyobject){
 
+console.log(`Username is ${anyobject.username} and price is ${anyobject.price}` );
 
-
-function loginUserMsg (username = "sam"){
-// if (username === undefined){
-if (!username){
-    console.log("Please enter a username");
-    return
 
 }
 
-return `${username} just logged in.`
+// handleObject(user)
+
+handleObject({
+    username: "sam",
+    price: 399,
+})  
 
 
+
+
+const  MynewArray = [200, 400, 600, 8000]
+
+function returnSecondValue(getArray){
+
+    return getArray[1]
 }
-console.log (loginUserMsg(""))
-console.log (loginUserMsg())
+
+// console.log(returnSecondValue(MynewArray));
+console.log(returnSecondValue([200, 400, 600, 8000]));
