cff94d9f775807d9a5619e45335a316c346e0a7f
akhil911-rs <clayjenseen72@gmail.com>
2026-04-09 09:58:37 +0000

    loops

---
+++ 4-iterations/five.js
@@ -0,0 +1,55 @@
+const coding = ["js", "rb", "py", "java", "cpp"]
+
+
+
+// coding.forEach( function (item) {
+// console.log(item);
+
+// } )
+
+
+
+// coding.forEach( (item) => {
+// console.log(item);
+
+// })
+
+
+// function printMe (item) {
+// console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+
+// coding.forEach(  (item, index, arr) => {
+
+//     console.log(item, index, arr);
+// })
+
+
+
+
+const mycoding = [
+{
+    langName: "Javascript",
+    langFileName: "js",
+},
+{
+    langName: "Java",
+    langFileName: "java",
+},
+{
+    langName: "python",
+    langFileName: "py",
+}
+
+]
+
+mycoding.forEach( (item) => {
+
+  
+    
+    console.log(item.langFileName);
+
+}  )
---
+++ 4-iterations/four.js
@@ -0,0 +1,42 @@
+const myObject = {
+
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift: 'swift by apple',
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+    
+}
+
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    //console.log(programming[key]);
+    
+}
+
+
+
+//maps are non itratable in object
+
+// const map = new Map ()
+// map.set('IN', "India")
+// map.set('ISR', "Israel")
+// map.set('POL', "Poland")
+// map.set('IN', "India")
+
+
+
+
+// for (const key in map) {
+
+//     console.log(map);
+    
+    
+    
+// }
